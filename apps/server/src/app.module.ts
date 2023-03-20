68a779f3193ae924d6969ad458f12c06482a79e6
Nir Kahana <nir@moveohls.com>
2023-03-20 13:58:27 +0200

    added docker-compose file

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { CatsModule } from './resources/cats/cats.module';
 import { HealthModule } from './resources/health/health.module';
@@ -6,7 +7,7 @@ import { HealthModule } from './resources/health/health.module';
 export const AppModules = [CatsModule, HealthModule];
 
 @Module({
-  imports: [...AppModules],
+  imports: [...AppModules, MongooseModule.forRoot(process.env.DATABASE_URI)],
   controllers: [],
   providers: [],
 })
